e6713f969c4b0633e171aa50215c41f457427188
Carlos Pineda <cara_p@unicah.edu>
2022-12-15 07:36:38 -0600

    Migraciones con SQL

--- aprendiendo-laravel/database/migrations/2022_12_15_042309_create_usuarios_table.php
+++ aprendiendo-laravel/database/migrations/2022_12_15_042309_create_usuarios_table.php
@@ -3,9 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,19 +13,30 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
-            //
-            $table->increments('id');
-            $table->string('nombre', 255);
-            $table->string('email', 255);
-            $table->string('password', 255);
-            $table->integer('edad');
-            $table->integer('sueldo');
-            $table->integer('salarios');
-            $table->timestamps();
+//        Schema::create('usuarios', function (Blueprint $table) {
+//            //
+//            $table->increments('id');
+//            $table->string('nombre', 255);
+//            $table->string('email', 255);
+//            $table->string('password', 255);
+//            $table->integer('edad');
+//            $table->integer('sueldo');
+//            $table->integer('salarios');
+//            $table->timestamps();
+//
+//
+//        });
+
+        // Crear migración con sql
 
 
-        });
+        DB::statement("create table usuarios(
+            id int(255) auto_increment not null,
+        nombre varchar(255),
+        email varchar(255),
+        password varchar(255),
+        PRIMARY KEY(id)
+    );");
     }
 
     /**
@@ -37,10 +48,15 @@ return new class extends Migration
     {
         Schema::drop('usuarios');
     }
+    // Crear migración
+    // php artisan make:migration create_usuarios_table --table=usuarios
 
     // Ejecutar todas las migraciones
     // php artisan migrate
 
     // Revertir cambios realizados por la migración
     // php artisan rollback
+
+    // Actualizar cambios hechos en la db
+    // pho artisan migration:refresh
 };
