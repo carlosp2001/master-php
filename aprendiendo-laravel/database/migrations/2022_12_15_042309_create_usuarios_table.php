35897433f1d05c8fb401d569f34598225eade0f1
Carlos Pineda <cara_p@unicah.edu>
2022-12-15 07:26:14 -0600

    Migraciones con Laravel

---
+++ aprendiendo-laravel/database/migrations/2022_12_15_042309_create_usuarios_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('nombre', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->integer('edad');
+            $table->integer('sueldo');
+            $table->integer('salarios');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('usuarios');
+    }
+
+    // Ejecutar todas las migraciones
+    // php artisan migrate
+
+    // Revertir cambios realizados por la migración
+    // php artisan rollback
+};
