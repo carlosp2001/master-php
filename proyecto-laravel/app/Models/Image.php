a4c2d2471a2d55396bbcec7645f00c4ace20fb9d
Carlos Pineda <cara_p@unicah.edu>
2022-12-24 11:01:52 -0600

    Configurar entidades y relación ORM Laravel

--- proyecto-laravel/app/Models/Image.php
+++ proyecto-laravel/app/Models/Image.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $table = 'images';
+
+    // Relacion uno a muchos / un solo modelo de imagen tendra muchos comentarios
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    // Relación de uno a muchos / un solo modelo de imagen tendrá muchos likes
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
+
+    // Relacion de muchos a uno / un mismo usuario puede crear muchas imagenes
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
--- proyecto-laravel/app/Models/Like.php
+++ proyecto-laravel/app/Models/Like.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $table = 'likes';
+
+    // Relación de muchos a uno
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    // Relación de muchos a uno
+    public function image()
+    {
+        return $this->belongsTo('App\Image', 'image_id');
+    }
 }
