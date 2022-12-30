95fb49d0260569629d3fd28ae67c4aff4efcbede
Carlos Pineda <cara_p@unicah.edu>
2022-12-30 11:28:57 -0600

    Listado de Imagenees

--- proyecto-laravel-5v/resources/views/home.blade.php
+++ proyecto-laravel-5v/resources/views/home.blade.php
@@ -5,19 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @include('includes.message')
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            @foreach($images as $image)
+            <div class="card pub_image">
+                <div class="card-header">
+                    @if($image->user->image)
+                    <div class="container-avatar">
+                        <img src="{{ route('user.avatar', ['filename' => $image->user->image]) }}"
+                             alt=""
+                             class="avatar">
+                    </div>
+                    @endif
+                    <div class="data-user">
+
+
+                    {{$image->user->name. ' ' . $image->user->surname . ' | @' . $image->user->nick}}
+                    </div>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
                     You are logged in!
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
