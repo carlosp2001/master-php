aaff890f55f24be489f8f000c61c29b23a319228
Carlos Pineda <cara_p@unicah.edu>
2023-01-02 11:26:09 -0600

    Formatear fechas con helpers y providers y formaulario de comentarios

--- proyecto-laravel-5v/resources/views/image/detail.blade.php
+++ proyecto-laravel-5v/resources/views/image/detail.blade.php
@@ -5,39 +5,50 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 @include('includes.message')
-                    <div class="card pub_image">
-                        <div class="card-header">
-                            @if($image->user->image)
-                                <div class="container-avatar">
-                                    <img src="{{ route('user.avatar', ['filename' => $image->user->image]) }}"
-                                         alt=""
-                                         class="avatar">
-                                </div>
-                            @endif
-                            <div class="data-user">
-                                {{$image->user->name. ' ' . $image->user->surname }}
-                                <span class="nickname">{{' | @' . $image->user->nick}}</span>
+                <div class="card pub_image pub_image_detail">
+                    <div class="card-header">
+                        @if($image->user->image)
+                            <div class="container-avatar">
+                                <img src="{{ route('user.avatar', ['filename' => $image->user->image]) }}"
+                                     alt=""
+                                     class="avatar">
                             </div>
-                        </div>
-
-                        <div class="card-body">
-                            <div class="image-container">
-                                <img src="{{ route('image.file', ['filename' => $image->image_path]) }}" alt="">
-                            </div>
-
-                            <div class="description">
-                                <span class="nickname">{{'@' . $image->user->nick}}</span>
-                                <p>{{$image->description}}</p>
-                            </div>
-                            <div class="likes">
-                                <img src="{{asset('img/heart-black.png')}}" alt="">
-                            </div>
-                            <div class="comments">
-                                <a href="" class="btn btn-warning btn-comments">Comentarios ({{count($image->comments)}}
-                                    )</a>
-                            </div>
+                        @endif
+                        <div class="data-user">
+                            {{$image->user->name. ' ' . $image->user->surname }}
+                            <span class="nickname">{{' | @' . $image->user->nick}}</span>
                         </div>
                     </div>
+
+                    <div class="card-body">
+                        <div class="image-container image-detail">
+                            <img src="{{ route('image.file', ['filename' => $image->image_path]) }}" alt="">
+                        </div>
+
+                        <div class="description">
+                            <span class="nickname">{{'@' . $image->user->nick}}</span>
+                            <span
+                                class="nickname date">{{' | ' . \FormatTime::LongTimeFilter( $image->created_at)}}</span>
+                            <p>{{$image->description}}</p>
+                        </div>
+                        <div class="likes">
+                            <img src="{{asset('img/heart-black.png')}}" alt="">
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="comments">
+                            <h2>Comentarios</h2>
+                            <hr>
+                            <form action="" method="post">
+                                @csrf
+                                <input type="hidden" value="" name="image_id" value="{{ $image->id }}">
+                                <p>
+                                    <textarea class="form-control" name="content" required></textarea>
+                                </p>
+                                <button type="submit" class="btn btn-success">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
