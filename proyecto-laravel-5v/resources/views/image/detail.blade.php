7e80b6de4ff67a69234eea14a0f34fe7af8861de
Carlos Pineda <cara_p@unicah.edu>
2023-01-02 13:25:03 -0600

    Listar comentarios de las fotografias

--- proyecto-laravel-5v/resources/views/image/detail.blade.php
+++ proyecto-laravel-5v/resources/views/image/detail.blade.php
@@ -51,6 +51,15 @@
                                 </p>
                                 <button type="submit" class="btn btn-success">Enviar</button>
                             </form>
+                            <hr>
+                            @foreach($image->comments as $comment)
+                                <div class="comment">
+                                        <span class="nickname">{{'@' . $comment->user->nick}}</span>
+                                        <span
+                                            class="nickname date">{{' | ' . \FormatTime::LongTimeFilter( $comment->created_at)}}</span>
+                                        <p>{{$comment->content}}</p>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
