26ec01686b830c7ddcf569397a91fc896a951b1f
Carlos Pineda <cara_p@unicah.edu>
2023-01-03 15:49:07 -0600

    Cambiar el color del boton like

--- proyecto-laravel-5v/public/js/main.js
+++ proyecto-laravel-5v/public/js/main.js
@@ -1,4 +1,29 @@
 window.addEventListener("load", function () {
-    alert("La pagina esta completamente cargada!");
-    alert("JS Cargado");
+    $('.btn-like').css('cursor', 'pointer');
+    $('.btn-dislike').css('cursor', 'pointer');
+
+    // Boton Like
+    function like() {
+        $('.btn-like').unbind('click').click(function () {
+            console.log('like');
+            $(this).addClass('btn-dislike').removeClass('btn-like');
+            $(this).attr('src', 'img/heart-black.png');
+            dislike();
+        });
+
+    }
+    like();
+
+        // Boton Dislike
+    function dislike() {
+        $('.btn-dislike').unbind('click').click(function () {
+            console.log('dislike');
+            $(this).addClass('btn-like').removeClass('btn-dislike');
+            $(this).attr('src', 'img/heart-red.png');
+            like();
+        });
+
+    }
+
+
 });
