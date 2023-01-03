32a13298a825358561ee4ee76fee1cd906fd8a3f
Carlos Pineda <cara_p@unicah.edu>
2023-01-03 15:00:54 -0600

    Metodo Like

---
+++ proyecto-laravel-5v/app/Http/Controllers/LikeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Like;
+
+class LikeController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function like($image_id) {
+        // Recoger datos del usuario y la imagen
+        $user = \Auth::user();
+
+        // Condicion para saber si el like ya existe y no duplicarlo
+        $isset_like = Like::where('user_id', $user->id)->where('image_id', $image_id)->count();
+        if ($isset_like == 0) {
+            $like = new Like();
+            $like->user_id = $user->id;
+            $like->image_id = (int)$image_id;
+
+            // Guardar
+            $like->save();
+            return response()->json([
+                'like' => $like
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'El like ya existe'
+            ]);
+        }
+    }
+
+    public function dislike($image_id) {
+
+    }
+}
