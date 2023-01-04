493ecd009df15386999a37edf81365fcf5f4f749
Carlos Pineda <cara_p@unicah.edu>
2023-01-04 10:30:22 -0600

    Mostrar datos en el perfil

--- proyecto-laravel-5v/resources/views/user/profile.blade.php
+++ proyecto-laravel-5v/resources/views/user/profile.blade.php
@@ -4,11 +4,28 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @foreach($user->images as $image)
-                    @include('includes.image', ['image' => $image])
-                @endforeach
-            </div>
+                <div class="data-user">
+                        @if($user->image)
+                            <div class="container-avatar">
+                                <img src="{{ route('user.avatar', ['filename' => $user->image]) }}"
+                                     alt=""
+                                     class="avatar">
+                            </div>
+                        @endif
 
+                    <div class="user-info">
+                        <h1>{{'@' . $user->nick}}</h1>
+                        <h2>{{$user->name . ' ' . $user->surname}}</h2>
+                        <p>{{ ' Se unió hace ' . \FormatTime::LongTimeFilter( $user->created_at) }}</p>
+                    </div>
+                </div>
+
+            </div>
+            @foreach($user->images as $image)
+                @include('includes.image', ['image' => $image])
+            @endforeach
         </div>
+
+    </div>
     </div>
 @endsection
