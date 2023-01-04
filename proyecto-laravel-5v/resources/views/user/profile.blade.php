952f350b8cf0040851b7ec7bf8b31163bb61e3e9
Carlos Pineda <cara_p@unicah.edu>
2023-01-04 11:15:50 -0600

    Maquetación del perfil

--- proyecto-laravel-5v/resources/views/user/profile.blade.php
+++ proyecto-laravel-5v/resources/views/user/profile.blade.php
@@ -4,23 +4,30 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="data-user">
-                        @if($user->image)
-                            <div class="container-avatar">
-                                <img src="{{ route('user.avatar', ['filename' => $user->image]) }}"
-                                     alt=""
-                                     class="avatar">
-                            </div>
-                        @endif
+                <div class="profile-user">
+                    @if($user->image)
+                        <div class="container-avatar">
+                            <img src="{{ route('user.avatar', ['filename' => $user->image]) }}"
+                                 alt=""
+                                 class="avatar">
+                        </div>
+                    @endif
 
                     <div class="user-info">
                         <h1>{{'@' . $user->nick}}</h1>
                         <h2>{{$user->name . ' ' . $user->surname}}</h2>
                         <p>{{ ' Se unió hace ' . \FormatTime::LongTimeFilter( $user->created_at) }}</p>
                     </div>
-                </div>
+                    <div class="clear-fix">
+                    </div>
 
+                </div>
+                <hr>
             </div>
+
+            <div class="clear-fix">
+            </div>
+
             @foreach($user->images as $image)
                 @include('includes.image', ['image' => $image])
             @endforeach
