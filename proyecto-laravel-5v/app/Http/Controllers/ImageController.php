701a0c9d6d48abb8830940f04ad4d8ee382f298b
Carlos Pineda <cara_p@unicah.edu>
2023-01-05 12:51:33 -0600

    Modal de Bootstrap 4, Formulario de edicion de imagenes y actualización de imagenes agregado

--- proyecto-laravel-5v/app/Http/Controllers/ImageController.php
+++ proyecto-laravel-5v/app/Http/Controllers/ImageController.php
@@ -107,4 +107,48 @@ class ImageController extends Controller
         return redirect()->route('home')->with($message);
 
     }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $image = Image::find($id);
+
+        if ($user && $image && $image->user->id == $user->id) {
+            return view('image.edit', ['image' => $image]);
+        } else {
+            return redirect()->route('home');
+        }
+    }
+
+    public function update(Request $request)
+    {
+        // Validar datos
+        $validate = $this->validate($request, [
+            'description' => 'required',
+            'image_path' => 'mimes:jpg,jpeg,png,gif'
+//            'image_path' => 'required|image'
+        ]);
+
+        // Recolectar datos
+        $image_id = $request->input('image_id');
+        $image_path = $request->file('image_path');
+        $description = $request->input('description');
+
+        // Conseguir objeto image en base de datos
+        $image = Image::find($image_id);
+        $image->description = $description;
+
+        // Subir fichero
+        if ($image_path) {
+            $image_path_name = time() . $image_path->getClientOriginalName();
+            Storage::disk('images')->put($image_path_name, File::get($image_path));
+            $image->image_path = $image_path_name;
+        }
+
+        // Actualizar registro
+        $image->update();
+
+        return redirect()->route('image.detail', ['id' => $image_id])->with(['message' => 'Imagen actualizada con exito']);
+
+    }
 }
--- proyecto-laravel-5v/resources/views/image/detail.blade.php
+++ proyecto-laravel-5v/resources/views/image/detail.blade.php
@@ -51,10 +51,46 @@
                         </div>
 
                         @if(Auth::user() && Auth::user()->id == $image->user->id)
-                        <div class="actions">
-                            <a href="" class="btn btn-primary">Actualizar</a>
-                            <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-danger">Borrar</a>
-                        </div>
+                            <div class="actions">
+                                <a href="{{ route('image.edit', ['id' => $image->id]) }}" class="btn btn-primary ml-2">Actualizar</a>
+{{--                                <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-danger">Borrar</a>--}}
+
+                                <!-- Button to Open the Modal -->
+                                <button type="button" class="btn btn-danger" data-toggle="modal"
+                                        data-target="#myModal">
+                                    Eliminar
+                                </button>
+
+                                <!-- The Modal -->
+                                <div class="modal" id="myModal">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+
+                                            <!-- Modal Header -->
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Estas seguro?</h4>
+                                                <button type="button" class="close" data-dismiss="modal">&times;
+                                                </button>
+                                            </div>
+
+                                            <!-- Modal body -->
+                                            <div class="modal-body">
+                                                Si eliminas esta imagen nunca podras recuperarla, estas seguro de quere borrarla?
+                                            </div>
+
+                                            <!-- Modal footer -->
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-success" data-dismiss="modal">
+                                                    Cancelar
+                                                </button>
+                                                <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-danger">Borrar definitivamente</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
                         @endif
                         <div class="clearfix"></div>
                         <div class="comments">
