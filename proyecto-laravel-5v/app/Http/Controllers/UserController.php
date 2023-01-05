73ed3f8e620330922fe0aa8a713ab2d6c3ff73a2
Carlos Pineda <cara_p@unicah.edu>
2023-01-05 13:36:49 -0600

    Metodo del buscador

--- proyecto-laravel-5v/app/Http/Controllers/UserController.php
+++ proyecto-laravel-5v/app/Http/Controllers/UserController.php
@@ -18,9 +18,16 @@ class UserController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index($search = null)
     {
-        $users = User::orderBy('id', 'desc')->paginate(5);
+        if (!empty($search)) {
+            $users = User::where('nick', 'LIKE', '%' . $search . '%')
+                ->orWhere('name', 'LIKE', '%' . $search . '%')
+                ->orWhere('surname', 'LIKE', '%' . $search . '%')
+                ->orderBy('id', 'desc')->paginate(5);
+        } else {
+            $users = User::orderBy('id', 'desc')->paginate(5);
+        }
         return view('user.index', [
             'users' => $users
         ]);
